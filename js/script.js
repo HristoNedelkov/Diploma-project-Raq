eb78d43eb01b020d415eea016081a06fb8700c95
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2022-03-28 19:21:26 +0300

    Clean the code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log(posts)
+const postBox = document.getElementsByClassName("posts")[0];
+const card = ({ name, imageSrc, description, price }) => {
+  return `
+  <div id="container">
+  <div class="product-details">
+  <h1>${name}</h1>
+  <p class="information">${description}</p>
+  <div class="control">
+  <button class="btn">
+  <span class="price">${price ? price : "999"} $ </span>
+  <span class="shopping-cart">
+  <i class="fa fa-shopping-cart" aria-hidden="true"></i
+  ></span>
+  <span class="buy">Buy Now</span>
+  </button>
+  </div>
+  </div>
+  <div class="product-image">
+  <img src="../Images/${imageSrc}" alt="Omar Dsoky" />
+  </div>
+  </div>`;
+};
+
+for (const el of posts) {
+  postBox.innerHTML += card(el);
+}
